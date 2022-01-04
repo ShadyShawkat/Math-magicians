ffe2eba48fa23e86facda7c2bb154e84c66a0393
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-04 22:09:02 +0200

    Change App.js into a class-based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React from 'react';
 import Calculator from './components/Calculator/Calculator';
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className={styles.wrapper}>
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
