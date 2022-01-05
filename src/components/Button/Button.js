1dfda2184834ede6cf22da4f9b6218eccf4c5fc4
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-05 17:57:38 +0200

    Refactor Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,28 +1,22 @@
-/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable object-curly-newline */
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-class Button extends React.Component {
-  render() {
-    const { children, isZero, isOperator, btnClickHandler } = this.props;
-    return (
-      <button
-        type="button"
-        className={`
+const Button = ({ children, isZero, isOperator, btnClickHandler }) => (
+  <button
+    type="button"
+    className={`
           ${styles.btn} 
           ${isZero ? styles['zero-num'] : ''} 
           ${isOperator ? styles.operator : ''}
         `}
-        onClick={btnClickHandler}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+    onClick={btnClickHandler}
+  >
+    {children}
+  </button>
+);
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
